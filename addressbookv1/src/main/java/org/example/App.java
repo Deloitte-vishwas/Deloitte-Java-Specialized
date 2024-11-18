6e1da1692fb95abf13c5bda9f828c8ee27bd5ffe
Deloitte-vishwas <vishwas32451@outlook.com>
2024-11-18 16:01:56 +0530

    spring xml configuration

---
+++ addressbookv1/src/main/java/org/example/App.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+
+        User u1 = (User) ctx.getBean("user");
+        u1.setUserName("vishwas");
+        u1.setEmailId("vishwas@cloudthat.com");
+        System.out.println( u1 );
+    }
+}
